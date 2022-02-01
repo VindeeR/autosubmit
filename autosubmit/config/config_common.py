c534712024bb68d66953000340a71bfdf4ec1460
dbeltran <daniel.beltran@bsc.es>
2022-02-01 12:25:28 +0100

    Small bug fix for migrate

--- autosubmit/config/config_common.py
+++ autosubmit/config/config_common.py
@@ -312,7 +312,7 @@ class AutosubmitConfig(object):
         :return: migrate user to
         :rtype: str
         """
-        return self._platforms_parser.get_option(section, 'HOST', '').lower()
+        return self._platforms_parser.get_option(section, 'HOST', '')
 
     def get_current_project(self, section):
         """
@@ -321,7 +321,7 @@ class AutosubmitConfig(object):
         :return: migrate user to
         :rtype: str
         """
-        return self._platforms_parser.get_option(section, 'PROJECT', '').lower()
+        return self._platforms_parser.get_option(section, 'PROJECT', '')
 
     def set_new_user(self, section, new_user):
         """
@@ -388,7 +388,7 @@ class AutosubmitConfig(object):
         :return: migrate project to
         :rtype: str
         """
-        return self._platforms_parser.get_option(section, 'PROJECT_TO', '').lower()
+        return self._platforms_parser.get_option(section, 'PROJECT_TO', '')
 
     def get_migrate_host_to(self, section):
         """
@@ -397,7 +397,7 @@ class AutosubmitConfig(object):
         :return: host_to
         :rtype: str
         """
-        return self._platforms_parser.get_option(section, 'HOST_TO', "none").lower()
+        return self._platforms_parser.get_option(section, 'HOST_TO', "none")
 
     def set_new_project(self, section, new_project):
         """

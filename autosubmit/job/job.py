fbc02f6ae0962dd5536c0c3c10ab613e86df1b13
dbeltran <daniel.beltran@bsc.es>
2022-08-29 13:40:57 +0200

    Added 5min retrial in case that something is wrong while recovering the As_conf info inside a thread.

--- autosubmit/job/job.py
+++ autosubmit/job/job.py
@@ -614,15 +614,27 @@ class Job(object):
         max_logs = 0
         sleep(5)
         stat_file = self.script_name[:-4] + "_STAT_"
+        retries = 2
+        count = 0
+        success = False
+        error_message = ""
+        while count < retries or success:
+            try:
+                as_conf = AutosubmitConfig(expid, BasicConfig, ConfigParserFactory())
+                as_conf.reload()
+                submitter = self._get_submitter(as_conf)
+                submitter.load_platforms(as_conf)
+                success = True
+            except BaseException as e:
+                error_message = str(e)
+                sleep(60*5)
+                pass
+            count=count+1
+        if not success:
+            raise AutosubmitError("Couldn't load the autosubmit platforms, seems that the local platform has some issue\n:{0}".format(error_message),6006)
+        platform = submitter.platforms[platform_name.lower()]
         try:
-            as_conf = AutosubmitConfig(expid, BasicConfig, ConfigParserFactory())
-            as_conf.reload()
-            submitter = self._get_submitter(as_conf)
-            submitter.load_platforms(as_conf)
-            platform = submitter.platforms[platform_name.lower()]
-
             platform.test_connection()
-
             max_logs = int(as_conf.get_retrials()) - fail_count
             last_log = int(as_conf.get_retrials()) - fail_count
             if self.wrapper_type is not None and self.wrapper_type == "vertical":
@@ -644,7 +656,7 @@ class Job(object):
 
         except BaseException as e:
             Log.printlog(
-                "{0} \n Couldn't connect to the remote platform for this {1} job err/out files. ".format(e.message, self.name), 6001)
+                "{0} \n Couldn't connect to the remote platform for {1} job err/out files. ".format(e.message, self.name), 6001)
         out_exist = False
         err_exist = False
         retries = 3

c8d09f5f2171458d35735d5b3d3ff1f591ced683
dbeltran <daniel.beltran@bsc.es>
2022-11-23 14:05:28 +0100

    configparser added to setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     keywords=['climate', 'weather', 'workflow', 'HPC'],
     install_requires=['argparse>=1.2,<2','six>=1.10.0', 'python-dateutil>2', 'pydotplus>=2', 'pyparsing>=2.0.1',
                       'numpy<1.17', 'matplotlib', 'typing', 'paramiko == 2.7.1',
-                      'mock>=1.3.0', 'portalocker==0.5.7', 'networkx', 'bscearth.utils', 'Xlib == 0.21', 'requests'],
+                      'mock>=1.3.0', 'portalocker==0.5.7', 'networkx', 'bscearth.utils', 'Xlib == 0.21', 'requests','configparser'],
     extras_require={
         'dialog': ["python2-pythondialog>=3.3.0"]
     },

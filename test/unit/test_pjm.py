26e39f6e9c87522104d47672b97e16d093f2e692
dbeltran <daniel.beltran@bsc.es>
2023-03-22 16:40:46 +0100

    testing pipeline change
    (cherry picked from commit 3deeaa46fec6d91f8af793e6061e942da3510e02)

--- test/unit/test_pjm.py
+++ test/unit/test_pjm.py
@@ -36,18 +36,12 @@ class TestPJM(TestCase):
         self.as_conf.experiment_data = dict()
         self.as_conf.experiment_data["DEFAULT"] = dict()
         self.as_conf.experiment_data["DEFAULT"]["HPCARCH"] = "ARM"
-        yml_file = Path("files/fake-jobs.yml")
-        if not yml_file.exists():
-            yml_file = Path("test/files/fake-jobs.yml")
-
+        yml_file = Path(__file__).resolve().parent / "files/fake-jobs.yml"
         factory = YAMLParserFactory()
         parser = factory.create_parser()
         parser.data = parser.load(yml_file)
         self.as_conf.experiment_data.update(parser.data)
-        yml_file = Path("files/fake-platforms.yml")
-        yml_file.exists()
-        if not yml_file.exists():
-            yml_file = Path("test/files/fake-platforms.yml")
+        yml_file = Path(__file__).resolve().parent / "files/fake-platforms.yml"
         factory = YAMLParserFactory()
         parser = factory.create_parser()
         parser.data = parser.load(yml_file)

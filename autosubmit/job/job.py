e298f3905b579295482ed6bcb6ef62f52fa1fb4e
dbeltran <daniel.beltran@bsc.es>
2023-12-04 11:05:43 +0100

    type python

--- autosubmit/job/job.py
+++ autosubmit/job/job.py
@@ -1732,7 +1732,7 @@ class Job(object):
         type_ = str(as_conf.jobs_data.get(self.section,{}).get("TYPE", "bash")).lower()
         if type_ == "bash":
             self.type = Type.BASH
-        elif type_ == "python":
+        elif self.type == Type.PYTHON3 or self.type == Type.PYTHON:
             self.type = Type.PYTHON
         elif type_ == "r":
             self.type = Type.R

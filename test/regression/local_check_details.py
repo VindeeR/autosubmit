3b3666d9a9c6e7cce89982229af433dec53acfbd
dbeltran <daniel.beltran@bsc.es>
2024-01-22 14:01:39 +0100

    updated test

--- test/regression/local_check_details.py
+++ test/regression/local_check_details.py
@@ -6,7 +6,8 @@ Works under local_computer TODO introduce in CI
 import os
 import subprocess
 BIN_PATH = '../../bin'
-VERSION = "test"
+EXPERIMENTS_PATH = '/home/dbeltran/autosubmit_docs'
+VERSION = 4.0
 
 def check_cmd(command, path=BIN_PATH):
     try:
@@ -18,8 +19,10 @@ def check_cmd(command, path=BIN_PATH):
     return output, error
 
 def run_test(expid):
-    #check_cmd(f"rm -r /home/dbeltran/new_autosubmit/{expid}/tmp/LOG_{expid}/*")
-    output = check_cmd(f"../../bin/autosubmit create {expid} -np -v -d;")
+    if VERSION == 4.0:
+        output = check_cmd(f"../../bin/autosubmit create {expid} -np -v -d -cw;")
+    else:
+        output = check_cmd(f"../../bin/autosubmit create {expid} -np -v -d -cw -f;")
     return output
 def perform_test(expids):
     to_exclude = []
@@ -32,7 +35,7 @@ def perform_test(expids):
             output = output.split("Job list created successfully[0m[39m")[1]
             output = expid + output
             # put it in a single file
-            with open(f"{VERSION}_multi_test.txt", "a") as myfile:
+            with open(f"{VERSION}_docs_test.txt", "a") as myfile:
                 myfile.write(output)
         except:
             to_exclude.append(expid)
@@ -40,16 +43,17 @@ def perform_test(expids):
     print(to_exclude)
 
 
-open(f"{VERSION}_multi_test.txt", "w").close()
+open(f"{VERSION}_docs_test.txt", "w").close()
 
 # list all experiments under ~/new_autosubmit.
 # except the excluded ones, which are not run
 expids = []
-excluded = ['a026','a00s','a01y', 'a00j', 'a020', 'a01t', 'a00q', 'a00f', 'a01h', 'a00o', 'a01c', 'a00z', 't008', 'a00y', 'a00r', 't009', 'a000', 'a01e', 'a01i', 'a002', 'a008', 'a010', 'a003', 't007', 'a01d', 'autosubmit.db', 'a021', 'a00h', 'as_times.db', 'a04d', 'a02v']
-for experiment in os.listdir("/home/dbeltran/new_autosubmit"):
+#excluded = ['a026', 'a01y', 'a00j', 'a020', 'a01t', 'a00q', 'a00f', 'a01h', 'a00o', 'a01c', 'a00z', 't008', 'a00y', 'a00r', 't009', 'a000', 'a01e', 'a01i', 'a002', 'a008', 'a010', 'a003', 't007', 'a01d', 'autosubmit.db', 'a021', 'a00h', 'as_times.db', 'a04d', 'a02v']
+excluded = []
+for experiment in os.listdir(f"{EXPERIMENTS_PATH}"):
     if experiment.startswith("a") or experiment.startswith("t") and len(experiment) == 4:
         if experiment not in excluded:
             expids.append(experiment)
 # Force
-expids = ["a001"]
+# expids = ["a001"]
 perform_test(expids)

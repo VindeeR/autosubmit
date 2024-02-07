72e73dc38d7a420f3f2cdf7c0a76dc29f0e375fc
dbeltran <daniel.beltran@bsc.es>
2024-02-07 14:46:50 +0100

    added unit test

--- test/unit/test_wrappers.py
+++ test/unit/test_wrappers.py
@@ -1554,13 +1554,166 @@ class TestWrappers(TestCase):
             self.assertEqual(min_h, 3)
 
 
-    # def test_check_jobs_to_run_first(self):
+    def test_check_jobs_to_run_first(self):
         # want to test self.job_packager.check_jobs_to_run_first(package)
+        date_list = ["d1"]
+        member_list = ["m1", "m2"]
+        chunk_list = [1, 2, 3, 4]
+        for section, s_value in self.workflows['basic']['sections'].items():
+            self.as_conf.jobs_data[section] = s_value
+        self._createDummyJobs(
+            self.workflows['basic'], date_list, member_list, chunk_list)
 
-    # def test_calculate_wrapper_bounds(self):
+        self.job_list.get_job_by_name(
+            'expid_d1_m1_s1').status = Status.COMPLETED
+        self.job_list.get_job_by_name(
+            'expid_d1_m2_s1').status = Status.COMPLETED
+
+        self.job_list.get_job_by_name('expid_d1_m1_1_s2').status = Status.READY
+        self.job_list.get_job_by_name('expid_d1_m2_1_s2').status = Status.READY
+
+        wrapper_expression = "s2 s3"
+        d1_m1_1_s2 = self.job_list.get_job_by_name('expid_d1_m1_1_s2')
+        d1_m1_2_s2 = self.job_list.get_job_by_name('expid_d1_m1_2_s2')
+        d1_m1_3_s2 = self.job_list.get_job_by_name('expid_d1_m1_3_s2')
+        d1_m1_4_s2 = self.job_list.get_job_by_name('expid_d1_m1_4_s2')
+        d1_m2_1_s2 = self.job_list.get_job_by_name('expid_d1_m2_1_s2')
+        d1_m2_2_s2 = self.job_list.get_job_by_name('expid_d1_m2_2_s2')
+        d1_m2_3_s2 = self.job_list.get_job_by_name('expid_d1_m2_3_s2')
+        d1_m2_4_s2 = self.job_list.get_job_by_name('expid_d1_m2_4_s2')
+
+        d1_m1_1_s3 = self.job_list.get_job_by_name('expid_d1_m1_1_s3')
+        d1_m1_2_s3 = self.job_list.get_job_by_name('expid_d1_m1_2_s3')
+        d1_m1_3_s3 = self.job_list.get_job_by_name('expid_d1_m1_3_s3')
+        d1_m1_4_s3 = self.job_list.get_job_by_name('expid_d1_m1_4_s3')
+        d1_m2_1_s3 = self.job_list.get_job_by_name('expid_d1_m2_1_s3')
+        d1_m2_2_s3 = self.job_list.get_job_by_name('expid_d1_m2_2_s3')
+        d1_m2_3_s3 = self.job_list.get_job_by_name('expid_d1_m2_3_s3')
+        d1_m2_4_s3 = self.job_list.get_job_by_name('expid_d1_m2_4_s3')
+
+        self.job_list._ordered_jobs_by_date_member["WRAPPERS"]["d1"] = dict()
+        self.job_list._ordered_jobs_by_date_member["WRAPPERS"]["d1"]["m1"] = [d1_m1_1_s2, d1_m1_1_s3, d1_m1_2_s2,
+                                                                              d1_m1_2_s3,
+                                                                              d1_m1_3_s2, d1_m1_3_s3, d1_m1_4_s2,
+                                                                              d1_m1_4_s3]
+
+        self.job_list._ordered_jobs_by_date_member["WRAPPERS"]["d1"]["m2"] = [d1_m2_1_s2, d1_m2_1_s3, d1_m2_2_s2,
+                                                                              d1_m2_2_s3,
+                                                                              d1_m2_3_s2, d1_m2_3_s3, d1_m2_4_s2,
+                                                                              d1_m2_4_s3]
+
+        self.job_packager.jobs_in_wrapper = wrapper_expression
+
+        self.job_packager.retrials = 0
+        # test vertical-wrapper
+        self.job_packager.wrapper_type["WRAPPER_V"] = 'vertical'
+        self.job_packager.current_wrapper_section = "WRAPPER_V"
+        self.as_conf.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section] = {}
+        self.as_conf.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["TYPE"] = "vertical"
+        self.as_conf.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["JOBS_IN_WRAPPER"] = "S2 S3"
+        package_m1_s2_s3 = [d1_m1_1_s2, d1_m1_1_s3, d1_m1_2_s2, d1_m1_2_s3]
+
+        packages_v = [JobPackageVertical(package_m1_s2_s3, configuration=self.as_conf)]
+        self.job_packager._jobs_list.jobs_to_run_first = []
+        for p in packages_v:
+            p2, run_first = self.job_packager.check_jobs_to_run_first(p)
+            self.assertEqual(p2.jobs, p.jobs)
+            self.assertEqual(run_first, False)
+        self.job_packager._jobs_list.jobs_to_run_first = [d1_m1_1_s2, d1_m1_1_s3]
+        for p in packages_v:
+            p2, run_first = self.job_packager.check_jobs_to_run_first(p)
+            self.assertEqual(p2.jobs, [d1_m1_1_s2, d1_m1_1_s3])
+            self.assertEqual(run_first, True)
+
+    def test_calculate_wrapper_bounds(self):
         # want to test self.job_packager.calculate_wrapper_bounds(section_list)
+        self.job_packager.current_wrapper_section = "WRAPPER"
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section] = {}
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["TYPE"] = "vertical"
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["JOBS_IN_WRAPPER"] = "S2 S3"
+        section_list = ["S2", "S3"]
+        # default wrapper limits
+        wrapper_limits = {'max': 9999999,
+                          'max_by_section': {'S2': 9999999, 'S3': 9999999},
+                          'max_h': 9999999,
+                          'max_v': 9999999,
+                          'min': 2,
+                          'min_h': 1,
+                          'min_v': 2
+                          }
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+        self.job_packager._as_config.experiment_data["WRAPPERS"]["MIN_WRAPPED"] = 3
+        self.job_packager._as_config.experiment_data["WRAPPERS"]["MAX_WRAPPED"] = 5
+        self.job_packager._as_config.experiment_data["WRAPPERS"]["MIN_WRAPPED_H"] = 2
+        self.job_packager._as_config.experiment_data["WRAPPERS"]["MIN_WRAPPED_V"] = 3
+        self.job_packager._as_config.experiment_data["WRAPPERS"]["MAX_WRAPPED_H"] = 4
+        self.job_packager._as_config.experiment_data["WRAPPERS"]["MAX_WRAPPED_V"] = 5
 
-    # def test_check_packages_respect_wrapper_policy(self):
+        wrapper_limits = {'max': 5*4,
+                          'max_by_section': {'S2': 5*4, 'S3': 5*4},
+                          'max_h': 4,
+                          'max_v': 5*4,
+                          'min': 3,
+                          'min_h': 2,
+                          'min_v': 3
+                          }
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["TYPE"] = "horizontal"
+        wrapper_limits = {'max': 5*4,
+                          'max_by_section': {'S2': 5*4, 'S3': 5*4},
+                          'max_h': 4*5,
+                          'max_v': 5,
+                          'min': 3,
+                          'min_h': 2,
+                          'min_v': 3
+                          }
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["TYPE"] = "horizontal-vertical"
+        wrapper_limits = {'max': 5*4,
+                          'max_by_section': {'S2': 5*4, 'S3': 5*4},
+                          'max_h': 4,
+                          'max_v': 5,
+                          'min': 3,
+                          'min_h': 2,
+                          'min_v': 3
+                          }
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["TYPE"] = "vertical-horizontal"
+        wrapper_limits = {'max': 5*4,
+                            'max_by_section': {'S2': 5*4, 'S3': 5*4},
+                            'max_h': 4,
+                            'max_v': 5,
+                            'min': 3,
+                            'min_h': 2,
+                            'min_v': 3
+                            }
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["MIN_WRAPPED"] = 3
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["MAX_WRAPPED"] = 5
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["MIN_WRAPPED_H"] = 2
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["MIN_WRAPPED_V"] = 3
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["MAX_WRAPPED_H"] = 4
+        self.job_packager._as_config.experiment_data["WRAPPERS"][self.job_packager.current_wrapper_section]["MAX_WRAPPED_V"] = 5
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+        del self.job_packager._as_config.experiment_data["WRAPPERS"]["MIN_WRAPPED"]
+        del self.job_packager._as_config.experiment_data["WRAPPERS"]["MAX_WRAPPED"]
+        del self.job_packager._as_config.experiment_data["WRAPPERS"]["MIN_WRAPPED_H"]
+        del self.job_packager._as_config.experiment_data["WRAPPERS"]["MIN_WRAPPED_V"]
+        del self.job_packager._as_config.experiment_data["WRAPPERS"]["MAX_WRAPPED_H"]
+        del self.job_packager._as_config.experiment_data["WRAPPERS"]["MAX_WRAPPED_V"]
+        returned_wrapper_limits = self.job_packager.calculate_wrapper_bounds(section_list)
+        self.assertDictEqual(returned_wrapper_limits, wrapper_limits)
+
+    def test_check_packages_respect_wrapper_policy(self):
         # want to test self.job_packager.check_packages_respect_wrapper_policy(built_packages_tmp,packages_to_submit,max_jobs_to_submit,wrapper_limits)
 
     #def test_build_packages(self):

2f140ce8a704528e84d0af9810ded74cdda9a7f1
dbeltran <daniel.beltran@bsc.es>
2024-08-08 08:55:57 +0200

    Changed tmp_path

--- test/unit/test_utils.py
+++ test/unit/test_utils.py
@@ -6,19 +6,28 @@ from pathlib import Path
 from autosubmit.helpers.utils import ASLock
 from autosubmitconfigparser.config.basicconfig import BasicConfig
 from log.log import AutosubmitCritical
-class MockBasicConfig(BasicConfig):
-    def read(self):
-        self.LOCAL_ROOT_DIR = "/tmp"
+
 
 @pytest.fixture
-def mock_basic_config(monkeypatch):
+def utils_tmp_path(tmp_path_factory):
+    return tmp_path_factory.mktemp("utils")
+
+class MockBasicConfig(BasicConfig):
+
+    def read(self):
+        pass
+
+@pytest.fixture
+def mock_basic_config(monkeypatch, utils_tmp_path):
+    MockBasicConfig.LOCAL_ROOT_DIR = utils_tmp_path
     monkeypatch.setattr("autosubmit.helpers.utils.BasicConfig", MockBasicConfig)
+    return MockBasicConfig
 
 def test_aslock(mock_basic_config):
     expid = "test_expid"
     current_hostname = socket.gethostname()
     current_pid = os.getpid()
-    lock_file_path = Path("/tmp") / expid / "tmp" / "autosubmit.lock"
+    lock_file_path = Path(MockBasicConfig.LOCAL_ROOT_DIR) / expid / "tmp" / "autosubmit.lock"
 
     # Ensure the directory exists
     lock_file_path.parent.mkdir(parents=True, exist_ok=True)

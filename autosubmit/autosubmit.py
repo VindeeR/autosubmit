f5f78980388ec36f34800e6a7f5e259daf61af9c
Albert Puiggros <albert.puiggros@bsc.es>
2024-12-09 11:51:47 +0100

    Indentation fixed

--- autosubmit/autosubmit.py
+++ autosubmit/autosubmit.py
@@ -2444,7 +2444,7 @@ class Autosubmit:
                 # Create rocrate object if requested
                 rocrate_data = as_conf.experiment_data.get("ROCRATE", None)
                 if rocrate_data:
-                        Autosubmit.provenance(expid, rocrate=True)
+                    Autosubmit.provenance(expid, rocrate=True)
         except BaseLockException:
             terminate_child_process(expid)
             raise

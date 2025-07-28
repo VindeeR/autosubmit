f244cc4db363d4f09e162ef6ec813c1c4d017023
VindeeR <ericklopes.47@gmail.com>
2025-07-28 15:23:14 +0200

    f

--- docs/source/env.sh
+++ docs/source/env.sh
@@ -1,6 +1,8 @@
 # shellcheck disable=SC2155
 export AUTOSUBMIT_CONFIGURATION=$(mktemp -d)
 
+touch "$AUTOSUBMIT_CONFIGURATION"/.autosubmitrc
+
 export AS_CONFIG="[database]
 path = $AUTOSUBMIT_CONFIGURATION/autosubmit
 filename = autosubmit.db
@@ -20,10 +22,14 @@ path = $AUTOSUBMIT_CONFIGURATION/autosubmit/metadata/data
 [historiclog]
 path = $AUTOSUBMIT_CONFIGURATION/autosubmit/metadata/logs"
 
-cat <<EOF > "$AUTOSUBMIT_CONFIGURATION"/.autosubmitrc
+cat <<EOF > ~/.autosubmitrc
 $AS_CONFIG
 EOF
 
 mkdir "$AUTOSUBMIT_CONFIGURATION"/autosubmit
 
+autosubmit configure
+
 autosubmit install
+
+autosubmit expid -d "test descript"
